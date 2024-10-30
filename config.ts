c0ff6efedad88310c2c1b6e294ed932748522681
KishiTheMechanic <s.kishi@elsoul.nl>
2024-10-30 14:21:13 +0100

    using skeeter.dev

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const appInfo = {
   copyright: 'ELSOUL LABO B.V.',
-  domain: 'fresh-web.skeet.dev',
+  domain: 'fresh-web.skeeter.dev',
   twitterId: '@SkeetDev',
   githubRepo: 'elsoul/skeet-v3-fresh-edge-web-rc',
   discordInviteURL: 'https://discord.gg/H2HeqRq54J',
